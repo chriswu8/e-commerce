0a1bd66ac5fdb0f4e7fc19559e78f5b8bb14f377
Chris Wu <zezemint@gmail.com>
2022-05-30 23:36:10 -0700

    added preliminary racfe in index.js and deleted Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      {/* a component */}
+      HeroBanner
+
+      <div>
+        <h2> Best Selling Products</h2>
+        <p> Some incredible description</p>
+      </div>
+
+      <div>
+        {['Product 1', 'Product 2'].map(
+          (product) => product)}
+      </div>
+
+      {/* a component */}
+      Footer
+    </>
   )
 }
 
-export default index
+export default Home
