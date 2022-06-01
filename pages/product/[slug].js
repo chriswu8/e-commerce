492ac483f2e497cdb50eaca4bda3848c066dc86a
Chris Wu <zezemint@gmail.com>
2022-06-01 11:43:29 -0700

    added an Add to Cart button and Buy Now button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -53,6 +53,15 @@ const ProductDetails = ({ product, products }) => {
                                 </span>
                             </p>
                         </div>
+                        <div className='buttons'>
+                            <button type='button' className='add-to-cart' onClick="">
+                                Add to Cart
+                            </button>
+
+                            <button type='button' className='buy-now' onClick="">
+                                Buy Now
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
